281681ba6305903ebc2a939b610e6b251fe5f8bd
huxhux <bostanovahux@outlook.com>
2026-06-30 20:46:38 +0100

    Added the base for the website

---
+++ index.js
@@ -0,0 +1,5 @@
+/* 
+The goal is to build a simple rock-paper-ssisors game, repson would input, the function would take that in
+and compare the input with randomly generated number, that would assined to rock/paper/ssisors.
+Else-if statements would determine the winner.
+*/
