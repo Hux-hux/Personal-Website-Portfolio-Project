02fd20ac02b3fa507639ebcc850c2052bd3bfe67
huxhux <bostanovahux@outlook.com>
2026-07-03 11:47:44 +0100

    Rules click event added

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ The goal is to build a simple rock-paper-ssisors game, repson would input, the f
 and compare the input with randomly generated number, that would assined to rock/paper/scissors.
 Else-if statements would determine the winner.
 */
+
+
+let scheme = document.getElementById('scheme');
+let toggleRules = document.getElementById('clickable');
+
+function showScheme() {
+    scheme.style.display = 'flex';
+    toggleRules.innerHTML = 'THE GAME RULES';
+}
+function stopShowingScheme() {
+    scheme.style.display = 'none';
+    toggleRules.innerHTML = 'SEE THE GAME RULES';
+}
+
+toggleRules.addEventListener('mousedown', showScheme);
+toggleRules.addEventListener('mouseup', stopShowingScheme);
