4ce7904ebd1d8ffb01b644c90a94b720a4744b26
huxhux <bostanovahux@outlook.com>
2026-07-06 12:01:21 +0100

    Added responsiveness and image scalability

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ Else-if statements would determine the winner.
 */
 
 
+//RULES
 let scheme = document.getElementById('scheme');
 let toggleRules = document.getElementById('clickable');
 
@@ -19,3 +20,45 @@ function stopShowingScheme() {
 
 toggleRules.addEventListener('mousedown', showScheme);
 toggleRules.addEventListener('mouseup', stopShowingScheme);
+
+
+
+//GAME
+let playersChoice = document.getElementById('choice');
+
+function ranNum() {
+    return Math.floor(Math.random() * 3); 
+}
+
+let compInput = (num) => {
+    if(num === 0) {
+        return 'rock';
+    } else if (num === 1) {
+        return 'paper';
+    } else if (num === 2) {
+        return 'scissors';
+    }
+}
+
+let aiChoice = compInput(ranNum());
+
+let userInput = document.querySelector('input[name="choice"]:checked').value;
+
+function compareInputs(playerInput, aiInput) {
+    if(playerInput === 'rock' && aiInput === 'scissors') {
+        return 'PLAYER WINS';
+    } else if(playerInput === 'rock' && aiInput === 'paper') {
+        return 'COMPUTER WINS';
+    } else if(playerInput === 'paper' && aiInput === 'scissors') {
+        return 'COMPUTER WINS';
+    } else if(playerInput === 'paper' && aiInput === 'rock') {
+        return 'PLAYER WINS';
+    } else if(playerInput === 'scissors' && aiInput === 'rock') {
+        return 'COMPUTER WINS';
+    } else if(playerInput === 'scissors' && aiInput === 'paper') {
+        return 'PLAYER WINS';
+    } else if (playerInput === aiInput) {
+        return "DRAW";
+    }
+}
+
