1b9a41eab27463f9007f14e0ea8e203cd5cc090e
huxhux <bostanovahux@outlook.com>
2026-07-07 12:32:33 +0100

    functionality added and tested, playable version

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ Else-if statements would determine the winner.
 let scheme = document.getElementById('scheme');
 let toggleRules = document.getElementById('clickable');
 
+
 function showScheme() {
     scheme.style.display = 'flex';
     toggleRules.innerHTML = 'THE GAME RULES';
@@ -24,41 +25,64 @@ toggleRules.addEventListener('mouseup', stopShowingScheme);
 
 
 //GAME
-let playersChoice = document.getElementById('choice');
+function runGame() {
 
-function ranNum() {
-    return Math.floor(Math.random() * 3); 
-}
-
-let compInput = (num) => {
-    if(num === 0) {
-        return 'rock';
-    } else if (num === 1) {
-        return 'paper';
-    } else if (num === 2) {
-        return 'scissors';
+    //computer output generator
+    function ranNum() {
+        return Math.floor(Math.random() * 3); 
     }
-}
 
-let aiChoice = compInput(ranNum());
-
-let userInput = document.querySelector('input[name="choice"]:checked').value;
-
-function compareInputs(playerInput, aiInput) {
-    if(playerInput === 'rock' && aiInput === 'scissors') {
-        return 'PLAYER WINS';
-    } else if(playerInput === 'rock' && aiInput === 'paper') {
-        return 'COMPUTER WINS';
-    } else if(playerInput === 'paper' && aiInput === 'scissors') {
-        return 'COMPUTER WINS';
-    } else if(playerInput === 'paper' && aiInput === 'rock') {
-        return 'PLAYER WINS';
-    } else if(playerInput === 'scissors' && aiInput === 'rock') {
-        return 'COMPUTER WINS';
-    } else if(playerInput === 'scissors' && aiInput === 'paper') {
-        return 'PLAYER WINS';
-    } else if (playerInput === aiInput) {
-        return "DRAW";
+    let compInput = (num) => {
+        if(num === 0) {
+            return 'rock';
+        } else if (num === 1) {
+            return 'paper';
+        } else if (num === 2) {
+            return 'scissors';
+        }
     }
+
+    let aiChoice = compInput(ranNum());
+
+    //input handling
+    let userInput = document.querySelector('input[name="choice"]:checked').value;
+
+    function compareInputs(playerInput, aiInput) {
+        if(playerInput === 'rock' && aiInput === 'scissors') {
+            return 'PLAYER WINS';
+        } else if(playerInput === 'rock' && aiInput === 'paper') {
+            return 'COMPUTER WINS';
+        } else if(playerInput === 'paper' && aiInput === 'scissors') {
+            return 'COMPUTER WINS';
+        } else if(playerInput === 'paper' && aiInput === 'rock') {
+            return 'PLAYER WINS';
+        } else if(playerInput === 'scissors' && aiInput === 'rock') {
+            return 'COMPUTER WINS';
+        } else if(playerInput === 'scissors' && aiInput === 'paper') {
+            return 'PLAYER WINS';
+        } else if (playerInput === aiInput) {
+            return "DRAW";
+        }
+    }
+
+    //result
+    let score = document.getElementById('winner');
+
+    let result = compareInputs(userInput, aiChoice);
+
+    score.innerHTML = result;
+
 }
+
+
+
+//GAME RUNNING
+let pressGo = document.getElementById('choice');
+
+pressGo.addEventListener('submit', function(event) {
+    event.preventDefault();
+    runGame();
+});
+
+
 
